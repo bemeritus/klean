23802ea6e74df768f2bccaccf44eda825a24bba3
onlyrsh <chankeilambda@gmail.com>
2024-01-11 18:13:43 +0500

    add:blade files

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PageController::class, 'main']);
+Route::get('about', [PageController::class, 'about'])->name('about');
+Route::get('service', [PageController::class, 'service'])->name('service');
+Route::get('project', [PageController::class, 'project'])->name('project');
+Route::get('contact', [PageController::class, 'contact'])->name('contact');
+
+Route::resource('posts', PostController::class);
